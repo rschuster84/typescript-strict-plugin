c79c035d954f75f00a9b071b315c2d6abca0dcd6
Roland Schuster <roland_schuster@gmx.de>
2025-11-18 18:01:19 +0100

    chore: add notStrict.ts with ts-strict-ignore comment

---
+++ sample-project/src/notStrict.ts
@@ -0,0 +1,14 @@
+// @ts-strict-ignore
+export {}; // make this file a module to avoid global redeclaration errors
+
+interface TestType {
+  bar: string;
+}
+
+const foo: TestType | null = null;
+
+const isTestType = (obj: any): obj is TestType => {
+  return obj != null && typeof obj === 'object' && obj.hasOwnProperty('bar') && typeof obj.bar === 'string';
+}
+
+const barValue: string = isTestType(foo) ? foo.bar : 'some default value';
